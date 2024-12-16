4d5b36ed834d51bbbcc141a448d046bed75b7bd0
tzewde <118752013+tzewde@users.noreply.github.com>
2024-12-16 00:40:59 -0500

    media.js

--- data/media.js
+++ data/media.js
@@ -5,19 +5,20 @@ import validation from './validation.js';
 let exportedMethods = {
     async getAllMovies() {
       const movieCollection = await movies();
-      const movieList = await movieCollection.find({}).sort({release_date: -1}).limit(100).toArray();
+      const movieList = await movieCollection.find({}).limit(1000).toArray();
       return movieList;
     },
     async getMovieById(movie_title) {
       //movie_title = validation.checkId(id);
       const movieCollection = await movies();
       const movie_current = await movieCollection.findOne({title: movie_title});
-      if (!movie_current) throw 'Error: User not found';
+      if (!movie_current) return false;
+      else //throw 'Error: User not found';
       return movie_current;
     },
     async getAllShows() {
         const showCollection = await shows();
-        const showList = await showCollection.find({}).sort({first_air_date: -1}).limit(100).toArray();
+        const showList = await showCollection.find({}).limit(1000).toArray();
         return showList;
       },
       async getShowById(show_title) {
@@ -27,11 +28,13 @@ let exportedMethods = {
         //return showList;
   
         //const show = await showCollection.findOne({_id: new ObjectId(id)});
-        if (!show_current) throw 'Error: Show not found';
+        if (!show_current) return false; //'Error: Show not found';
         //return [show_current.overview, show_current.first_air_date, show_current.vote_average, show_current.genres];
+        else
         return show_current;
       },
-      async addMovie(title, overview, vote_average, release_date) {
+      async addMovie(newTitle, overview, vote_average, release_date) {
+        if (!newTitle) throw "Title must be provided";
         overview = validation.checkString(overview, 'overview');
         //release_date = validation.checkString(release_date, 'release_date');
         //vote_average = validation.checkString(vote_average, 'vote_average');
@@ -45,10 +48,23 @@ let exportedMethods = {
         };
   
         const movieCollection = await movies();
+        //const existingMovieName = await userCollection.findOne({ title: newTitle });
+        const existingMovieName = await getMovieById(newTitle);
+        if (existingMovieName) {
+            throw "There is already a Movie with that Title";
+        }
+
         const newInsertInformation = await movieCollection.insertOne(newMovie);
-        if (!newInsertInformation.insertedId) throw 'Error: Insert failed!';
-    
-        return await this.getMovieById(newInsertInformation.insertedId.toString());
+        //if (!newInsertInformation.insertedId) throw 'Error: Insert failed!';
+        if (!newInsertInformation.acknowledged || !newInsertInformation.insertedId) {
+          return {registrationCompleted: false};
+        } else {
+          return {registrationCompleted: true};
+        }
+
+
+
+        //return await this.getMovieById(newInsertInformation.insertedId.toString());
       },   
       async addShow(name, overview, vote_average, first_air_date) {
         overview = validation.checkString(overview, 'overview');
