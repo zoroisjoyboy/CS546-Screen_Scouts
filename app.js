9437b15593d07f8382a46d8342047c052ec6c7a0
victheboygenius <vicpat419@gmail.com>
2024-12-16 00:35:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 import express from 'express';
 import session from 'express-session';
 import exphbs from 'express-handlebars';
-//import myMiddleware from './middleware.js';
-
-const app = express();
+import handlebars from 'handlebars';
 import configRoutesFunction from './routes/index.js';
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+handlebars.registerHelper('eq', function(a, b) {
+     return a === b;
+});
+
+app.use(express.static('public'));
 app.use('/public', express.static('public'));
+app.use('/assets', express.static('public/assets'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.use(session({
   name: 'AuthenticationState',
   secret: 'some secret string!',
+  resave: false,
   saveUninitialized: false,
-  resave: false
-})
-);
+}));
 
-
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 configRoutesFunction(app);
 
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3000');
-  });
-
+  console.log('Server running on port 3000');
+});
