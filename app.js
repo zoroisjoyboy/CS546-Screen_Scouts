25be28df663c801e5f8658f39bb851a6e95d283e
Allen Saakov <152702163+zoroisjoyboy@users.noreply.github.com>
2024-12-15 19:53:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,10 @@
 // });
 
 import express from 'express';
-
+import session from 'express-session';
+import exphbs from 'express-handlebars';
+import handlebars from 'handlebars';
+import configRoutesFunction from './routes/index.js';
 
 const app = express();
 
@@ -61,9 +64,24 @@ handlebars.registerHelper('eq', function(a, b) {
      return a === b;
 });
 
+app.use(express.static('public'));
 app.use('/public', express.static('public'));
 app.use('/assets', express.static('public/assets'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(session({
+  name: 'AuthenticationState',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+configRoutesFunction(app);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
